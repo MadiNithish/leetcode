7edc5d97b5b843137d472636248084ba6240b7f9
MadiNithish <madinithish@gmail.com>
2026-07-08 16:04:45 +0530

    Time: 5 ms (30.65%), Space: 67.4 MB (18.87%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,25 +4,37 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    k = k%nums.length
-    nums.reverse()
+    // k = k%nums.length;
+    // nums.reverse();
 
-    let left = 0;
-    let right = k-1;
+    // let left = 0;
+    // let right = k-1;
     
-    while(left<right){
-        [nums[left],nums[right]]=[nums[right],nums[left]];
-        left++;
-        right--;
+    // while(left<right){
+    //     [nums[left],nums[right]]=[nums[right],nums[left]];
+    //     left++;
+    //     right--;
+    // }
+
+    // left = k;
+    // right = nums.length-1;
+
+    // while(left<right){
+    //     [nums[left],nums[right]]=[nums[right],nums[left]];
+    //     left++;
+    //     right--;
+    // }
+    k = k % nums.length;
+
+    function reverse(arr, left, right) {
+        while (left < right) {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
     }
 
-    left = k;
-    right = nums.length-1;
-
-    while(left<right){
-        [nums[left],nums[right]]=[nums[right],nums[left]];
-        left++;
-        right--;
-    }
-    
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
 };
