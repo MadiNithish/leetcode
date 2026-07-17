13b4c5391b64490d263ef0e8d193b53dbe4b6832
MadiNithish <madinithish@gmail.com>
2026-07-17 12:46:29 +0530

    Time: 0 ms (100%), Space: 53 MB (89.16%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -3,10 +3,29 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    for(let i = 1; i*i<=num; i++){
-        if(i*i===num){
+   // Because once i * i becomes greater than num, there's no point continuing.
+    // for(let i = 1; i*i<=num; i++){
+    //     if(i*i===num){
+    //         return true;
+    //     }
+    // }
+    // return false;
+    let left = 1;
+    let right = num;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let square = mid * mid;
+
+        if (square === num) {
             return true;
+        } else if (square < num) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-    return false
+
+    return false;
+
 };
