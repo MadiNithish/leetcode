b82f6d512ba00639ef15b2f256b6e4a03e9dc3e0
MadiNithish <madinithish@gmail.com>
2026-07-22 14:57:09 +0530

    Time: 9 ms (38.19%), Space: 62.6 MB (48.82%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,10 +3,31 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    let squares = []
-    for(let i=0; i<nums.length; i++){
-        squares.push(nums[i]*nums[i]);
+    // let squares = []
+    // for(let i=0; i<nums.length; i++){
+    //     squares.push(nums[i]*nums[i]);
+    // }
+    // squares.sort((a,b)=>a-b)
+    // return squares;
+
+    let result = []
+
+    let left = 0;
+    let right = nums.length-1;
+    let index = nums.length-1;
+
+    while(left<=right){
+        const leftSquare = nums[left]*nums[left];
+        const rightSquare = nums[right]*nums[right];
+
+        if(leftSquare>rightSquare){
+            result[index] = leftSquare;
+            left++;
+        }else{
+            result[index] = rightSquare;
+            right--;
+        }
+        index--;
     }
-    squares.sort((a,b)=>a-b)
-    return squares;
+    return result;
 };
