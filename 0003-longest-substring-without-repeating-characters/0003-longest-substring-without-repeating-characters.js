65b24ba6c1bb1e0436778402f143d23211b36b70
MadiNithish <madinithish@gmail.com>
2026-07-24 19:57:19 +0530

    Time: 5 ms (77.8%), Space: 58.2 MB (51.67%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const set = new Set();
+    let left = 0;
+    let maxLen = 0;
+    for(let right = 0; right<s.length; right++){
+        let ch = s[right];
+        while(set.has(ch)){
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(ch);
+        maxLen = Math.max(maxLen, right-left+1);
+    }
+    return maxLen;
+};
